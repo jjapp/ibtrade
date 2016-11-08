9df289e3baeb34bdfe65a94693c792b01dc2f20a
John Appert <appertjt@gmail.com>
2016-11-08 15:49:24 -0800

    added sample code for connecting to IB

---
+++ getter.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+'''
+file to get and print real time quotes from interactive brokers
+'''
+
+from ib.opt import Connection
+
+def print_message_from_ib(msg):
+    print(msg)
+
+def main():
+    conn = Connection.create(port=7496, clientId=100)
+    conn.registerAll(print_message_from_ib)
+    conn.connect()
+
+    #In future blog posts, this is where we'll write code that actually does
+    #something useful, like place orders, get real-time prices, etc.
+
+    import time
+    time.sleep(1) #Simply to give the program time to print messages sent from IB
+    conn.disconnect()
+
+if __name__ == "__main__": main()
